a7887e79e1ccc78fd03aef7cbef3ee5db2da5b9b
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2021-01-21 16:04:32 -0300

    DevBarber | Criando pasta de sql para backup da database criada pelo migrations

---
+++ sql/devbarber.sql
@@ -0,0 +1,293 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 21-Jan-2021 às 20:02
+-- Versão do servidor: 10.4.13-MariaDB
+-- versão do PHP: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `devbarber`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `barberavailability`
+--
+
+CREATE TABLE `barberavailability` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_barber` int(11) NOT NULL,
+  `weekday` int(11) NOT NULL,
+  `hours` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `barberphotos`
+--
+
+CREATE TABLE `barberphotos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_barber` int(11) NOT NULL,
+  `url` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `barberreviews`
+--
+
+CREATE TABLE `barberreviews` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_barber` int(11) NOT NULL,
+  `rate` double(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `barbers`
+--
+
+CREATE TABLE `barbers` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'default.png',
+  `stars` double(8,2) NOT NULL DEFAULT 0.00,
+  `longitude` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `latitude` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `barberservices`
+--
+
+CREATE TABLE `barberservices` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_barber` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` double(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `barbertestimonials`
+--
+
+CREATE TABLE `barbertestimonials` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_barber` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rate` double(8,2) NOT NULL,
+  `body` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2021_01_21_181005_create_all_tables', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `userappointments`
+--
+
+CREATE TABLE `userappointments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_barber` int(11) NOT NULL,
+  `ap_datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `userfavorites`
+--
+
+CREATE TABLE `userfavorites` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_barber` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'default.png',
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `barberavailability`
+--
+ALTER TABLE `barberavailability`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `barberphotos`
+--
+ALTER TABLE `barberphotos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `barberreviews`
+--
+ALTER TABLE `barberreviews`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `barbers`
+--
+ALTER TABLE `barbers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `barberservices`
+--
+ALTER TABLE `barberservices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `barbertestimonials`
+--
+ALTER TABLE `barbertestimonials`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `userappointments`
+--
+ALTER TABLE `userappointments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `userfavorites`
+--
+ALTER TABLE `userfavorites`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `barberavailability`
+--
+ALTER TABLE `barberavailability`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `barberphotos`
+--
+ALTER TABLE `barberphotos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `barberreviews`
+--
+ALTER TABLE `barberreviews`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `barbers`
+--
+ALTER TABLE `barbers`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `barberservices`
+--
+ALTER TABLE `barberservices`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `barbertestimonials`
+--
+ALTER TABLE `barbertestimonials`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `userappointments`
+--
+ALTER TABLE `userappointments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `userfavorites`
+--
+ALTER TABLE `userfavorites`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
