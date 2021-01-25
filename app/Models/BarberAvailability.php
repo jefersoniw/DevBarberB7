40e528df0982154a205c9e1a1a6501d5d89f2fa4
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2021-01-25 13:22:13 -0300

    DevBarber | Criei e configurei os models, criei os controllers

---
+++ app/Models/BarberAvailability.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarberAvailability extends Model
+{
+    use HasFactory;
+
+    protected $table = 'barberavailability';
+
+    public $timestamps = 'false';
+}
+
