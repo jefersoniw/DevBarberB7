3f1c6acff42b9c5e358c14d181023990365d9de2
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2021-01-27 15:23:29 -0300

    Devbarber | Criei filtro para pegar barbeiro por localização

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -48,10 +48,63 @@ class BarberController extends Controller
         return $array;
     }
 
-    public function list(){
+
+
+    private function searchGeo($address){
+        $key = env('MAPS_KEY', null);
+
+        $address = urlencode($address);
+
+        $url = 'https://maps.googleapis.com/maps/api/geocode/json?address='.$address.'&key'.$key;
+    
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+
+        return json_decode($res, true);
+    
+    }
+
+
+    public function list(Request $request){
         $array = ['error' => ''];
+
+        $lat = $request->input('lat');
+        $ln = $request->input('lng');
+        $city = $request->input('city');
+
+
+        if(!empty($city)){
+            $res = $this->searchGeo($city);
+
+            if(count($res['results']) > 0){
+                $lat = $res['results'][0]['geometry']['location']['lat'];
+                $lng = $res['results'][0]['geometry']['location']['lng'];
+            }
+        }elseif(!empty($lat) && !empty($lng)){
+            $res = $this->searchGeo($lat.','.$lng);
+
+            if(count($res['results']) > 0){
+                $city = $res['results'][0]['formatted_address'];
+            }
+
+        }else{
+            $lat = '-23.5489';
+            $lng = '-46.6388';
+            $city = 'São Paulo';
+        }
+
+
         
-        $barbers = Barber::all();
+        $barbers = Barber::select(Barber::raw('*, SQRT(
+            POW(69.1 * (latitude - '.$lat.'), 2) +
+            POW(69.1 * ('.$lng.' - longitude) * COS(latitude / 57.3), 2)) AS distance'))
+            ->havingRaw('distance < ?', [4000])
+            ->orderBy('distance', 'ASC')
+            ->get();
 
         foreach($barbers as $bkey => $bvalue){
             $barbers[$bkey]['avatar'] = url('media/avatars/'.$barbers[$bkey]['avatar']);
